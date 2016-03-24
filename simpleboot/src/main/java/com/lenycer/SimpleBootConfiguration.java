d907cfbf2c0bf81a90b370b7e322d76957958278
lenycer <lenycer@gmail.com>
2016-03-24 17:42:23 +0900

    simpleboot local git commit

---
+++ simpleboot/src/main/java/com/lenycer/SimpleBootConfiguration.java
@@ -0,0 +1,21 @@
+package com.lenycer;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.lenycer.sample.SampleBeans;
+
+/**
+ * @SpringBootApplication 에 @Configuration가 존재하지만 test 및 설정 유형별 관리를 위해 뺄 수도 있음.
+ * @author 현석
+ *
+ */
+@Configuration
+public class SimpleBootConfiguration {
+
+	@Bean
+	public SampleBeans sampleBeans() {
+		return new SampleBeans();
+	}
+}
