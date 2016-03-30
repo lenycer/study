2f084d0e04a40dc33e773ca9213f88cebf0c4dd6
lenycer <lenycer@gmail.com>
2016-03-30 13:24:07 +0900

    method

--- simpleboot/src/main/java/com/lenycer/SimpleController.java
+++ simpleboot/src/main/java/com/lenycer/SimpleController.java
@@ -3,11 +3,14 @@ package com.lenycer;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleController {
 	
+	@RequestMapping(value="/", method=RequestMethod.GET)
 	public Map<String, String> simple() {
 		Map<String, String> map = new HashMap<>();
 		map.put("lenycer", "test");
